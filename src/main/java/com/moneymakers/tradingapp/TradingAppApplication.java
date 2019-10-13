7986a46d5b1a33c24e4fe23e60bb480ceb9f229d
Arnolds Pukss <pukss.arnolds@gmail.com>
2019-10-13 22:10:19 +0300

    initial commit

---
+++ src/main/java/com/moneymakers/tradingapp/TradingAppApplication.java
@@ -0,0 +1,15 @@
+package com.moneymakers.tradingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingAppApplication.class, args);
+		System.out.println("adsdasd");
+	}
+
+
+}
